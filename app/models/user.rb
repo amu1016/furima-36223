0883ae4121e4b1b84eb9346bf97e51c33001d846
amu1016 <amu.prog@gmail.com>
2021-07-28 18:28:35 +0900

    エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: PASSWORD_REGEX, "is invalid. Include both letters and numbers."}
+  validates :password, format: { with: PASSWORD_REGEX, message: "is invalid. Include both letters and numbers."}
   with_options presence: true do
     validates :nickname
     with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
