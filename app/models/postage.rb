4998c99277f56b2b3dc16cab07765959f28bb6fd
amu1016 <amu.prog@gmail.com>
2021-07-29 16:43:35 +0900

    active_hash修正

--- app/models/postage.rb
+++ app/models/postage.rb
@@ -3,7 +3,6 @@ class Postage < ActiveHash::Base
     { id: 0, name: '---' },
     { id: 1, name: '着払い(購入者負担)' },
     { id: 2, name: '送料込み(出品者負担)' },
-    { id: 3, name: '目立った傷や汚れなし' },
   ]
  
    include ActiveHash::Associations
