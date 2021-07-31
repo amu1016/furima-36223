0ad58ac2b4698915ca325cae6fc8241dc276fc9e
amu1016 <amu.prog@gmail.com>
2021-07-31 17:22:41 +0900

    レビュー修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   
   def index
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    Item.destroy(params[:id])
+    @item.destroy
     redirect_to root_path
   end
 
